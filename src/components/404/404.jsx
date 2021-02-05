6d1c947662dc289bf3952b220334ed77fb0e9bfe
Roman Pogorelov <rmk140987@gmail.com>
2021-02-05 18:48:55 +0300

    2.1. Added routing for compontents.

--- src/components/404/404.jsx
+++ src/components/404/404.jsx
@@ -3,9 +3,14 @@ import {Link} from 'react-router-dom';
 
 const NotFound = () => (
   <>
-    <h1>404</h1>
-    <h1>Not Found</h1>
-    <Link to="/">Go to the main page</Link>
+    <div style={{
+      width: `250px`,
+      margin: `0 auto`
+    }}>
+      <h1>404</h1>
+      <h1>Not Found</h1>
+      <Link to="/">Go to the main page</Link>
+    </div>
   </>
 );
 
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,40 @@
 import React from "react";
 import {Main} from "../main/main";
 import PropTypes from "prop-types";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import {SignIn} from "../sign-in/sign-in";
+import {MyList} from "../my-list/my-list";
+import {Film} from "../film/film";
+import {AddReview} from "../add-review/add-review";
+import {NotFound} from "../404/404";
+import {Player} from "../player/player";
 
 const App = (props) => (
-  <>
-    <Main promo={props.promo}/>
-  </>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <Main promo={props.promo}/>
+      </Route>
+      <Route exact path="/login">
+        <SignIn/>
+      </Route>
+      <Route exact path="/mylist">
+        <MyList/>
+      </Route>
+      <Route exact path="/films/:id">
+        <Film/>
+      </Route>
+      <Route exact path="/films/:id/review">
+        <AddReview/>
+      </Route>
+      <Route exact path="/player/:id">
+        <Player/>
+      </Route>
+      <Route>
+        <NotFound/>
+      </Route>
+    </Switch>
+  </BrowserRouter>
 );
 
 App.propTypes = {

a3013abbab9b73ccb10635b1a386240537ded315
Roman Pogorelov <rmk140987@gmail.com>
2021-02-09 14:44:09 +0300

    3.1. Added film, review mocks.

---
+++ src/mocks/reviews.js
@@ -0,0 +1,103 @@
+const reviewMocks = [
+  {
+    "id": 1,
+    "film_id": 1,
+    "user": {
+      "id": 4,
+      "name": `name1`
+    },
+    "rating": 0.1,
+    "comment": `comment1`,
+    "date": `2019-05-08T14:13:56.569Z`
+  },
+  {
+    "id": 2,
+    "film_id": 2,
+    "user": {
+      "id": 4,
+      "name": `name2`
+    },
+    "rating": 0.2,
+    "comment": `comment2`,
+    "date": `2019-05-08T14:13:56.569Z`
+  },
+  {
+    "id": 3,
+    "film_id": 3,
+    "user": {
+      "id": 4,
+      "name": `name3`
+    },
+    "rating": 0.3,
+    "comment": `comment3`,
+    "date": `2019-05-08T14:13:56.569Z`
+  },
+  {
+    "id": 4,
+    "film_id": 4,
+    "user": {
+      "id": 4,
+      "name": `name4`
+    },
+    "rating": 0.4,
+    "comment": `comment4`,
+    "date": `2019-05-08T14:13:56.569Z`
+  },
+  {
+    "id": 5,
+    "film_id": 5,
+    "user": {
+      "id": 4,
+      "name": `name5`
+    },
+    "rating": 0.5,
+    "comment": `comment5`,
+    "date": `2019-05-08T14:13:56.569Z`
+  },
+  {
+    "id": 6,
+    "film_id": 6,
+    "user": {
+      "id": 4,
+      "name": `name6`
+    },
+    "rating": 0.6,
+    "comment": `comment6`,
+    "date": `2019-05-08T14:13:56.569Z`
+  },
+  {
+    "id": 7,
+    "film_id": 7,
+    "user": {
+      "id": 4,
+      "name": `name7`
+    },
+    "rating": 0.7,
+    "comment": `comment7`,
+    "date": `2019-05-08T14:13:56.569Z`
+  },
+  {
+    "id": 8,
+    "film_id": 8,
+    "user": {
+      "id": 4,
+      "name": `name8`
+    },
+    "rating": 0.8,
+    "comment": `comment8`,
+    "date": `2019-05-08T14:13:56.569Z`
+  },
+  {
+    "id": 9,
+    "film_id": 8,
+    "user": {
+      "id": 4,
+      "name": `name9`
+    },
+    "rating": 0.9,
+    "comment": `comment9`,
+    "date": `2019-05-08T14:13:56.569Z`
+  },
+];
+
+export {reviewMocks};

cffe14ae98ea60070ded687bee4fcc64c54044a1
Roman Pogorelov <rmk140987@gmail.com>
2021-02-10 07:09:55 +0300

    3.1. Refactored history prop to <Link>.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {filmValidation} from "../../validation";
 import * as PropTypes from "prop-types";
-import {useHistory} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const MovieCard = (props) => {
   const {film, onCover} = props;
-  const history = useHistory();
+  const href = `/films/${film.id}`;
 
   return <>
     <article className="small-movie-card catalog__movies-card" onMouseOver={() => onCover(film.id)}>
@@ -15,10 +15,7 @@ const MovieCard = (props) => {
           width="280" height="175"/>
       </div>
       <h3 className="small-movie-card__title">
-        <a className="small-movie-card__link" href="movie-page.html" onClick={(evt)=>{
-          evt.preventDefault();
-          history.push(`/films/${film.id}`);
-        }}>{film.name}</a>
+        <Link className="small-movie-card__link" to={href}>{film.name}</Link>
       </h3>
     </article>
   </>;

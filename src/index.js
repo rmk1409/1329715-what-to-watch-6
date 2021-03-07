820164b5ce6e22c459cfa4fc95e2a0d8a31fa334
Roman Pogorelov <rmk140987@gmail.com>
2021-03-07 20:32:36 +0300

    7.1. Added Redux toolkit(RTK).

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {App} from "./components/app/app";
-import {applyMiddleware, createStore} from "redux";
 import {combinedReducer} from "./store/reducer";
 import {Provider} from "react-redux";
-import {composeWithDevTools} from "redux-devtools-extension";
 import {createAPI} from "./services/api";
-import thunk from "redux-thunk";
 import {checkAuth, fetchFilmList} from "./store/api-actions";
 import {redirect} from "./store/redirect";
 import {LoadingScreen} from "./components/loading-screen/loading-screen";
 import {getFilmsByCurrentGenre, increaseShownFilmQuantity} from "./store/action";
+import {configureStore} from "@reduxjs/toolkit";
 
 const title = `The Grand Budapest Hotel`;
 const genre = `Drama`;
@@ -19,7 +17,16 @@ const date = 2014;
 const promo = {title, genre, date};
 
 const api = createAPI();
-const store = createStore(combinedReducer, composeWithDevTools(applyMiddleware(thunk.withExtraArgument(api)), applyMiddleware(redirect)));
+const store = configureStore({
+  reducer: combinedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api
+      },
+      redirect
+    })
+});
 
 Promise.resolve()
   .then(() => {

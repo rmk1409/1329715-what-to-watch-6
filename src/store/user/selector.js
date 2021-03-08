0be8047b4ceeffffda2750af765c8f8e09606d7e
Roman Pogorelov <rmk140987@gmail.com>
2021-03-08 16:14:24 +0300

    7.1. Removed selectors.

--- src/store/user/selector.js
+++
@@ -1,6 +0,0 @@
-import {NameSpace} from "../reducer";
-
-const getAuthorizationStatus = (state) => state[NameSpace.USER].authorizationStatus;
-const getAuthInfo = (state) => state[NameSpace.USER].authInfo;
-
-export {getAuthorizationStatus, getAuthInfo};

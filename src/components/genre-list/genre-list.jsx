00c7bf2d3db5fc7026e02aebfb4e25adb7f8e40e
Roman Pogorelov <rmk140987@gmail.com>
2021-03-09 11:25:54 +0300

    Limit max genre quantity.

--- src/components/genre-list/genre-list.jsx
+++ src/components/genre-list/genre-list.jsx
@@ -1,14 +1,14 @@
 import React, {useMemo} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {changeGenre, getFilmsByCurrentGenre, setShownFilmQuantity} from "../../store/action";
-import {Genre} from "../../const";
+import {Genre, MAX_GENRE_QUANTITY} from "../../const";
 import {NameSpace} from "../../store/reducer";
 
 const GenreList = () => {
   const {allFilms, chosenGenre} = useSelector((state) => state[NameSpace.DATA]);
   const uniqueGenres = useMemo(
-      () => Array.from(new Set(allFilms.map((film) => film.genre))),
-      [allFilms]
+    () => Array.from(new Set(allFilms.map((film) => film.genre))),
+    [allFilms],
   );
 
   const dispatch = useDispatch();
@@ -26,12 +26,12 @@ const GenreList = () => {
         key={Genre.ALL} className={`catalog__genres-item ${chosenGenre === Genre.ALL ? Genre.ACTIVE_GENRE_CLASS : ``}`}>
         <a href="#" className="catalog__genres-link" onClick={onClickGenre}>All genres</a>
       </li>
-      {uniqueGenres.map((currentUniqueGenre) =>
+      {uniqueGenres.map((currentUniqueGenre, index) => index < MAX_GENRE_QUANTITY ?
         <li
           key={currentUniqueGenre}
           className={`catalog__genres-item ${chosenGenre === currentUniqueGenre ? Genre.ACTIVE_GENRE_CLASS : ``}`}>
           <a href="#" className="catalog__genres-link" onClick={onClickGenre}>{currentUniqueGenre}</a>
-        </li>)}
+        </li> : ``)}
     </ul>
   </>;
 };

a0319b1ab5f874ddfa6ad59133038c20401c353f
Roman Pogorelov <rmk140987@gmail.com>
2021-03-09 11:34:11 +0300

    Fixed link to the main page.

--- src/components/my-list/my-list.jsx
+++ src/components/my-list/my-list.jsx
@@ -16,7 +16,7 @@ const MyList = () => {
     <div className="user-page">
       <header className="page-header user-page__head">
         <div className="logo">
-          <a href="main.html" className="logo__link">
+          <a href="/" className="logo__link">
             <span className="logo__letter logo__letter--1">W</span>
             <span className="logo__letter logo__letter--2">T</span>
             <span className="logo__letter logo__letter--3">W</span>
@@ -36,7 +36,7 @@ const MyList = () => {
 
       <footer className="page-footer">
         <div className="logo">
-          <a href="main.html" className="logo__link logo__link--light">
+          <a href="/" className="logo__link logo__link--light">
             <span className="logo__letter logo__letter--1">W</span>
             <span className="logo__letter logo__letter--2">T</span>
             <span className="logo__letter logo__letter--3">W</span>

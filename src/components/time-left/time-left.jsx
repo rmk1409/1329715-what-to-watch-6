62fb3b52a3ebc5dba97260f1b623a5b66898610d
Roman Pogorelov <rmk140987@gmail.com>
2021-03-09 11:22:33 +0300

    7.2. Added left time functionality.

---
+++ src/components/time-left/time-left.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import {MAX_SINGLE_DIGITAL_NUMBER, SECONDS_IN_HOUR, SECONDS_IN_MIN} from "../../const";
+
+const getFormattedTime = (totalSeconds) => {
+  const hours = Math.floor(totalSeconds / SECONDS_IN_HOUR);
+  let minutes = Math.floor((totalSeconds - hours * SECONDS_IN_HOUR) / SECONDS_IN_MIN);
+  minutes = minutes > MAX_SINGLE_DIGITAL_NUMBER ? minutes : `0${minutes}`;
+  let seconds = totalSeconds % SECONDS_IN_MIN;
+  seconds = seconds > MAX_SINGLE_DIGITAL_NUMBER ? seconds : `0${seconds}`;
+  return `${hours}:${minutes}:${seconds}`;
+};
+
+const TimeLeft = ({seconds}) => (
+  <>
+    <div className="player__time-value">{getFormattedTime(seconds)}</div>
+  </>
+);
+
+TimeLeft.propTypes = {
+  seconds: PropTypes.number.isRequired,
+};
+
+export {TimeLeft};
